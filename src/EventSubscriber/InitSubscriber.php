f554f3ea564f25b92ceefef51842fd3bad34be6f
Jason Pamental <jason@rwt.io>
2016-05-19 17:43:41 -0400

    initial commit of module port

---
+++ src/EventSubscriber/InitSubscriber.php
@@ -0,0 +1,32 @@
+<?php /**
+ * @file
+ * Contains \Drupal\typogrify\EventSubscriber\InitSubscriber.
+ */
+
+namespace Drupal\typogrify\EventSubscriber;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class InitSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [KernelEvents::REQUEST => ['onEvent', 0]];
+  }
+
+  public function onEvent() {
+    // @FIXME
+// The Assets API has totally changed. CSS, JavaScript, and libraries are now
+// attached directly to render arrays using the #attached property.
+// 
+// 
+// @see https://www.drupal.org/node/2169605
+// @see https://www.drupal.org/node/2408597
+// drupal_add_css(drupal_get_path('module', 'typogrify') . '/typogrify.css');
+
+  }
+
+}
---
+++ src/Filter.php
@@ -0,0 +1,4 @@
+<?php
+namespace Drupal\typogrify;
+
+class Filter {}
